a33806908eecd90ecf9c60d98a330508d2e4635c
Zosia Trela <ztrela@ucsc.edu>
2025-11-25 12:16:55 -0800

    adding how-to-play page

--- Assets/Scripts/StartScreenManager.cs
+++ Assets/Scripts/StartScreenManager.cs
@@ -8,4 +8,16 @@ public class StartScreenManager : MonoBehaviour
         // Load the main game scene
         SceneManager.LoadScene("Level 0");
     }
+
+    public void OnTutorialButtonClicked()
+    {
+        // Load tutorial
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void OnBackButtonClicked()
+    {
+        // Go back to main menu
+        SceneManager.LoadScene("Main Menu");
+    }
 }
