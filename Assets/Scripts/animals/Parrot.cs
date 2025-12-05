1296355eb51471ab5f3b6e0092c85f8a0e3ca2fd
Ana <162636836+actiasAnii@users.noreply.github.com>
2025-12-05 10:19:00 -0800

    new level and make parrot trigger pig animation

--- Assets/Scripts/animals/Parrot.cs
+++ Assets/Scripts/animals/Parrot.cs
@@ -46,6 +46,7 @@ public class Parrot : BaseAnimal
             if (hitAnimal.bounce)
             {
                 mirrored = !mirrored;
+                hitAnimal.OnBounced(dir);
             }
         }
 
